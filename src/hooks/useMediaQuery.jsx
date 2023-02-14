e31e087eb239560f43297f71ee26ccd9a97b9040
Akhilesh yadav <akhileshyadav310596@gmail.com>
2023-02-14 22:33:08 +0530

    update

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -1,21 +1,19 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-const useMediaQuery=(query)=>{
-    const [matches,setMatches]=useState(false)
+const useMediaQuery = (query) => {
+  const [matches, setMatches] = useState(false);
 
-    useEffect(()=>{
-        const media=window.matchMedia(query)
-        if(media.matches!==matches){
-            setMatches(media.matches);
-        }
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    window.addEventListener("resize", listener);
+    return () => window.removeEventListener("resize", listener);
+  }, [matches, query]);
 
-        const listtener=()=>setMatches(media.matches)
+  return matches;
+};
 
-        window.addEventListener("resize",listtener);
-        return ()=>window.removeEventListener("resize",listtener)
-    },[matches,query])
-
-    return matches
-}
-
-export default useMediaQuery
+export default useMediaQuery;
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
+import open from "../assets/menu-icon.svg"
+import close from "../assets/close-icon.svg"
 
 const Link = ({ page, selectedPage, setSelectedPage }) => {
   const lowerCasePage = page.toLowerCase();
@@ -17,20 +19,67 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
-    const [isMenuToggled, setIsMenuToggled] = useState(false);
-    const isDesktop = useMediaQuery("(min-width: 768px)");
-    const navbarBackground = isTopOfPage ? "" : "bg-red";
-  
-    return (
-      <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
-        <div className="flex items-center justify-between mx-auto w-5/6">
-          <h4 className="font-playfair text-3xl font-bold">AKY</h4>
-  
-          {/* DESKTOP NAV */}
-          {isDesktop ? (
-            <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
+
+  return (
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">AKY</h4>
+
+        {/* DESKTOP NAV */}
+        {isDesktop ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Resume"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img alt="menu-icon" src={open} />
+          </button>
+        )}
+
+        {/* MOBILE MENU POPUP */}
+        {!isDesktop && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src={close} />
+              </button>
+            </div>
+
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
               <Link
                 page="Home"
                 selectedPage={selectedPage}
@@ -57,58 +106,11 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                 setSelectedPage={setSelectedPage}
               />
             </div>
-          ) : (
-            <button
-              className="rounded-full bg-red p-2"
-              onClick={() => setIsMenuToggled(!isMenuToggled)}
-            >
-              <img alt="menu-icon" src="../assets/menu-icon.svg" />
-            </button>
-          )}
-  
-          {/* MOBILE MENU POPUP */}
-          {!isDesktop && isMenuToggled && (
-            <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
-              {/* CLOSE ICON */}
-              <div className="flex justify-end p-12">
-                <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                  <img alt="close-icon" src="../assets/close-icon.svg" />
-                </button>
-              </div>
-  
-              {/* MENU ITEMS */}
-              <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Skills"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Projects"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Resume"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-              </div>
-            </div>
-          )}
-        </div>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
